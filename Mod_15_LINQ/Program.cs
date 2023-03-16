55a2e6922df457340faf83a136dc50114620d330
ivan <gervadin.nomer1@gmail.com>
2023-03-16 10:48:30 +0300

    Добавьте файлы проекта.

---
+++ Mod_15_LINQ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace Mod_15_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Список учеников школы по классам:
+            var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+
+        }
+        //Метод LINQ.
+        static List<string> GetAllStudents(Classroom[] classes)
+        {
+            List<string> students = new List<string>();
+            var result = from student in classes
+                         select student.Students;
+
+            foreach (var student in result)
+            {
+                foreach (var s in student)
+                {
+                    students.Add(s);
+                }
+            }
+            return students;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
